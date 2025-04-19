a73cb41c5accc010c3b428f10f0d5b05890679f9
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-19 18:29:37 +0300

    create homepage urls

---
+++ personal_site/homepage/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from homepage import views
+
+app_name = "homepage"
+
+
+urlpatterns = [
+    path(
+        "",
+        views.get_homepage,
+        name="home",
+    ),
+]
