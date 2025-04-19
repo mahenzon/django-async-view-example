ee4d6524dddad35333045cf53eb289c52e6f9907
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-19 18:29:29 +0300

    create async homepage view

--- personal_site/homepage/views.py
+++ personal_site/homepage/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+async def get_homepage(request: HttpRequest) -> HttpResponse:
+    context = {
+        "items": [1, 2, 3],
+    }
+    return render(
+        request=request,
+        template_name="homepage/home.html",
+        context=context,
+    )
