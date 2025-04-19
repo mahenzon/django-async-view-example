55b4fdd6b93219f59c7e8ee8759c8151017acddf
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-19 18:19:22 +0300

    add Profile model to admin

--- personal_site/homepage/admin.py
+++ personal_site/homepage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from homepage.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "birth_date",
+        "city",
+        "bio",
+    )
